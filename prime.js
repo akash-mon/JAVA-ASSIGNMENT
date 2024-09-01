fcf73891f41e902574ec7179af03bc173b14289c
AKASH AJAY <84310711+akash-mon@users.noreply.github.com>
2024-09-01 23:01:34 +0530

    Add files via upload

---
+++ prime.js
@@ -0,0 +1,7 @@
+let a=[7,6,8,10];
+let x=a[0];
+if( x%2===0 || x%3===0 || x%5===0){
+    console.log(x+" is not prime");
+}else{
+    console.log(x+" is prime");
+}
---
+++ square.js
@@ -0,0 +1,10 @@
+let arr=[1,2,3,4];
+sum=0;
+for (let i = 0; i < arr.length; i++) {
+    
+    x=arr[i]*arr[i];
+    sum=sum+x;
+    
+}
+console.log(sum);
+ 
